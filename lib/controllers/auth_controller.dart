eb9dd179e6d422ff29ce59d16a8f0e6166c34b01
Michał Błachut <michal.blachut@autorun.solutions>
2023-05-28 22:26:39 +0200

    Added AuthController class for signing users

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> signUpUsers(String email, String fullName, String phoneNumber,
+      String password) async {
+    String res = "Some error";
+
+    try {
+      if (email.isNotEmpty &&
+          fullName.isNotEmpty &&
+          phoneNumber.isNotEmpty &&
+          password.isNotEmpty) {
+        //Create the user
+
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        res = "Please fill the fields";
+      }
+    } catch (e) {
+
+    }
+    return res;
+  }
+
+}
