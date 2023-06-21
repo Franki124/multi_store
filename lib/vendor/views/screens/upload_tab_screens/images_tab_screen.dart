6c9c4d947a6d8a21c383917a929516e89825a57d
Michał Błachut <michal.blachut@autorun.solutions>
2023-06-21 20:09:20 +0200

    Added display images for images_tab_screen.dart

--- lib/vendor/views/screens/upload_tab_screens/images_tab_screen.dart
+++ lib/vendor/views/screens/upload_tab_screens/images_tab_screen.dart
@@ -1,10 +1,59 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ImagesScreen extends StatelessWidget {
-  const ImagesScreen({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagesScreen extends StatefulWidget {
+  @override
+  State<ImagesScreen> createState() => _ImagesScreenState();
+}
+
+class _ImagesScreenState extends State<ImagesScreen> {
+  final ImagePicker picker = ImagePicker();
+
+  List<File> _image = [];
+
+  chooseImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile == null) {
+      print('no image picked');
+    } else {
+      setState(() {
+        _image.add(File(pickedFile.path));
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text("Images Screen");
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          GridView.builder(
+            shrinkWrap: true,
+              itemCount: _image.length + 1,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, mainAxisSpacing: 8, childAspectRatio: 3 / 3),
+              itemBuilder: (context, index) {
+                return index == 0
+                    ? Center(
+                        child: IconButton(
+                          onPressed: () {
+                            chooseImage();
+                          },
+                          icon: Icon(Icons.add),
+                        ),
+                      )
+                    : Container(
+                        decoration: BoxDecoration(
+                            image:
+                                DecorationImage(image: FileImage(_image[index - 1]))),
+                      );
+              })
+        ],
+      ),
+    );
   }
 }
