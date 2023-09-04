33bf5db5a48177b877cbdd372e3169001f8c3e88
Michał Błachut <michal.blachut@autorun.solutions>
2023-09-04 19:50:46 +0200

    Removed deprecated flutter_fire_ui and change it flutter hot!

--- lib/vendor/views/auth/vendor_auth.dart
+++ lib/vendor/views/auth/vendor_auth.dart
@@ -1,8 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutterfire_ui/auth.dart';
-
-import '../screens/landing_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:multi_store/vendor/views/screens/landing_screen.dart';
 
 class VendorAuthScreen extends StatelessWidget {
   const VendorAuthScreen({Key? key}) : super(key: key);
@@ -11,23 +10,19 @@ class VendorAuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
-      // If the user is already signed-in, use it as initial data
       initialData: FirebaseAuth.instance.currentUser,
       builder: (context, snapshot) {
-        // User is not signed in
         if (!snapshot.hasData) {
-          return const SignInScreen(
-              providerConfigs: [
-                EmailProviderConfiguration(),
-                GoogleProviderConfiguration(clientId: "1:247416658912:android:b487b4385b999decbce985"),
-                PhoneProviderConfiguration(),
-              ]
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+            ],
           );
         }
-
-        // Render your application if authenticated
         return LandingScreen();
       },
     );
   }
 }
+
+// 1:247416658912:android:b487b4385b999decbce985
